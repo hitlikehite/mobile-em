c345c531a99af94c7e83881c000b171cb477ff3d
HL <hailiangzhang@user.noreply.gitee.com>
2019-07-09 19:37:52 +0800

    登录页基本完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,63 @@
 <template>
-  <p>登陆界面</p>
+  <div>
+    <van-nav-bar title="登陆"/>
+    <form>
+      <van-cell-group>
+        <van-field
+          v-model="form.mobile"
+          required
+          clearable
+          label="用户名"
+          placeholder="请输入用户名"
+        />
+        <van-field
+          v-model="form.code"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          required
+        />
+      </van-cell-group>
+      <van-button type="info" @click.prevent="sendlogin" class="loginbtn">登陆</van-button>
+    </form>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  name: 'loginIndex',
+  data () {
+    return {
+      form: {
+        mobile: '15120013810',
+        code: '123456'
+      }
+    }
+  },
+  methods: {
+    async  sendlogin () {
+      try {
+        let res = await axios({
+          method: 'POST',
+          url: 'http://toutiao.course.itcast.cn/app/v1_0/authorizations',
+          data: this.form
+        })
+        console.log(res)
+        this.$toast('登陆成功')
+        this.$router.push({ name: 'home' })
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
+.loginbtn {
+  width: 345px;
+  text-align: center;
+  margin-left: 15px;
+}
 </style>
